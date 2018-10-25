74168098830edff9cc3097c7bdf9cfa24c7e4487
elvis caceres <e.caceres@fundacionpatino.org>
2018-10-25 16:34:20 -0400

    method info extension
    Added extension to get method by name and num of parameters

---
+++ ORG.FSIP.ERP.Libraries.Reflection/MethodInfoExtension.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+
+namespace ORG.FSIP.ERP.Libraries.Reflection
+{
+    public static class MethodInfoExtension
+    {
+        public static MethodInfo GetMethod(this Type type, string name, int parameterCount)
+        {
+            return type.GetMethods().Single(method => method.Name == name && method.GetParameters().Length == parameterCount);
+        }
+    }
+}
