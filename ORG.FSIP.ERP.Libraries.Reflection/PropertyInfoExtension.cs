bbb6d92cffb295c444470e7ff6c91363447115c0
elvis caceres <e.caceres@fundacionpatino.org>
2018-10-25 16:29:00 -0400

    property info extension
    Added extension for
     * check if a property exist in a class
     * get property type by name

---
+++ ORG.FSIP.ERP.Libraries.Reflection/PropertyInfoExtension.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Reflection;
+
+namespace ORG.FSIP.ERP.Libraries.Reflection
+{
+    public static class PropertyInfoExtension
+    {
+        public static bool HasProperty(this Type type, string propertyName)
+        {
+            return type.GetProperty(propertyName, BindingFlags.IgnoreCase | BindingFlags.Public | BindingFlags.Instance) != null;
+        }
+
+        public static Type PropertyType(this Type type, string propertyName)
+        {
+            return type.GetProperty(propertyName, BindingFlags.IgnoreCase | BindingFlags.Public | BindingFlags.Instance).PropertyType;
+        }
+                
+    }
+}
